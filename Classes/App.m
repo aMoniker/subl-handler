04517d2d3d50f27055af1377de59f259e94af131
Jim Greenleaf <james.a.greenleaf@gmail.com>
2012-12-10 06:57:39 +0900

    add support for opening multiple files to subl-handler

--- Classes/App.m
+++ Classes/App.m
@@ -1,6 +1,7 @@
 #import "App.h"
 
 #import "NSURL+L0URLParsing.h"
+#import "RegexKitLite.h"
 
 @implementation App
 
@@ -17,25 +18,52 @@ NSString *defaultPath = @"/Applications/Sublime Text 2.app/Contents/SharedSuppor
 -(void)handleGetURLEvent:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent {
     if (nil == path) path = defaultPath;
 
-    // txmt://open/?url=file://~/.bash_profile&line=11&column=2
     NSURL *url = [NSURL URLWithString:[[event paramDescriptorForKeyword:keyDirectObject] stringValue]];
 
     if (url && [[url host] isEqualToString:@"open"]) {
         NSDictionary *params = [url dictionaryByDecodingQueryString];
-        NSURL *file_url = [NSURL URLWithString:[params objectForKey:@"url"]];
+        NSMutableArray *all_files = [NSMutableArray array];
 
-        if (file_url && [[file_url scheme] isEqualToString:@"file"]) {
-            NSString *file = [file_url path];
-            NSString *line = [params objectForKey:@"line"];
-            NSString *column = [params objectForKey:@"column"];
-
-            if (file) {
-                NSTask *task = [[NSTask alloc] init];
-                [task setLaunchPath:path];
-                [task setArguments:[NSArray arrayWithObjects: [NSString stringWithFormat:@"%@:%lu:%lu", file, [line integerValue], [column integerValue]], nil]];
-                [task launch];
-                [task release];
+        NSString *url_string = [params objectForKey:@"url"];
+        NSString *files_string = [url_string stringByReplacingOccurrencesOfRegex:@"^file://" withString:@""];
+        NSString *root_dir = [files_string stringByMatching:@"^.+?//"];
+        
+        if (!root_dir) {
+            root_dir = @"";
+            [all_files addObject:files_string];
+        } else {
+            files_string = [files_string stringByReplacingOccurrencesOfRegex:@"^.+?//" withString:@""];
+            files_string = [@"/" stringByAppendingString:files_string];
+            root_dir = [root_dir stringByReplacingOccurrencesOfRegex:@"//$" withString:@""];
+            
+            NSArray *all_files_immutable = [files_string componentsSeparatedByRegex:@" "];
+            all_files = [all_files_immutable mutableCopy];
+        }
+        
+        root_dir = [@"file://" stringByAppendingString:root_dir];
+        
+        NSMutableArray *full_path_files = [NSMutableArray array];
+        
+        [all_files enumerateObjectsUsingBlock:^(id file, NSUInteger idx, BOOL *stop) {
+            NSURL *file_url = [NSURL URLWithString:[root_dir stringByAppendingString:file]];
+            
+            if (file_url && [[file_url scheme] isEqualToString:@"file"]) {
+                NSString *file = [file_url path];
+                NSString *line = [params objectForKey:@"line"];
+                NSString *column = [params objectForKey:@"column"];
+                
+                file_url = [NSString stringWithFormat:@"%@:%lu:%lu", file, [line integerValue], [column integerValue]];
+                
+                [full_path_files addObject:file_url];
             }
+        }];
+            
+        if ([full_path_files count] > 0) {
+            NSTask *task = [[NSTask alloc] init];
+            [task setLaunchPath:path];
+            [task setArguments:full_path_files];
+            [task launch];
+            [task release];
         }
     }
 
---
+++ subl-handler-redux/subl-handler-redux/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  subl-handler-redux
+//
+//  Created by Jim on 12/9/12.
+//  Copyright (c) 2012 Jim Greenleaf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
